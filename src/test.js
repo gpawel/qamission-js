a9c26980f77a5dc2fc448bae19cc1b67df265cd4
Pavel <gpawel@gmail.com>
2020-06-06 11:21:07 -0400

    init commit

---
+++ src/test.js
@@ -0,0 +1,16 @@
+function fib(n) {
+    var a, b;
+    a = 0;
+    b = 1;
+    while (a < n ){
+        //console.log('%d',a);
+        process.stdout.write(a.toString(10)+' ');
+        nextNumber = a + b;   
+        a = b;
+        b = nextNumber;       
+    }
+    console.log();
+}
+
+
+fib(50);
